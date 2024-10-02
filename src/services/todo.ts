c78f5b028e3dad03885f7ad2bbe9a5de0edb7a56
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-10-02 18:05:12 -0300

    FIX(postTodo): stringify task_name

--- src/services/todo.ts
+++ src/services/todo.ts
@@ -2,43 +2,47 @@ import { Task } from "../interfaces";
 import api from "../plugins/axios";
 
 class TodoService {
-    async getTodoList() {
-        try{
+    async getTodoList(): Promise<Task[] | []> {
+        try {
             const response = await api.get('');
             return <Task[]>response.data;
-        }catch(error){
+        } catch (error) {
             console.error(error);
         }
+        return [];
     }
+
     async postTodo(task_name: string) {
-        const postURL: string = `?task_name=${task_name}`;
-        console.log(`postURL: ${postURL}`);
-        try{
-            const response = await api.post(postURL);
+        try {
+            const response = await api.post('', JSON.stringify({ task_name }));
             console.log(response);
-            return response.data;
-        }catch(error){
+            return <Task>response.data;
+        } catch (error) {
             console.error(error);
         }
     }
+
     async deleteTodo(id: number) {
-        const deleteURL: string = `?id=${id}`;
+        const deleteURL: string = `/${id}`;
         console.log(`deleteURL: ${deleteURL}`);
-        try{
+        try {
             const response = await api.delete(deleteURL);
             console.log(response);
             return response.data;
-        }catch(error){
+        } catch (error) {
             console.error(error);
         }
     }
-    async patchTodo(id: number, newIsDoneStatus: boolean) {
-        const patchURL: string = `?id=${id}&isDone=${newIsDoneStatus}`;
-        console.log(`patchURL: ${patchURL}`);
-        try{
-            const response = await api.delete(patchURL);
+
+    async patchTodo(id: number, newIsDoneStatus: number) {
+        const patchURL: string = `/${id}`;
+        console.log({ patchURL, isDone: newIsDoneStatus });
+        console.log(JSON.stringify({ isDone: newIsDoneStatus }));
+
+        try {
+            const response = await api.delete(patchURL,);
             return response.data;
-        }catch(error){
+        } catch (error) {
             console.error(error);
         }
     }
